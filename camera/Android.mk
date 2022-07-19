1d6947dfe6a7ef19b28ce1b0b2e5caec4536f3bc
SpectreDev-007 <mohammad.galaxy07@gmail.com>
2022-07-19 18:18:29 +0100

    pdx206: fix module name

--- camera/Android.mk
+++ camera/Android.mk
@@ -1,6 +1,6 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
-LOCAL_MODULE := SomcPhotoPro
+LOCAL_MODULE := SemcCameraUI
 LOCAL_MODULE_TAGS := optional
 LOCAL_SRC_FILES := $(LOCAL_MODULE).apk
 LOCAL_MODULE_CLASS := APPS
